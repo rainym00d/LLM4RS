41d989ab2e591831aa556df00e4dcbf2ab7befdf
rainym00d <rainym00d@163.com>
2023-04-22 20:20:27 +0800

    D: delete test file

--- test.py
+++
@@ -1,8 +0,0 @@
-import json
-filename = "data/example_requests_to_parallel_process.jsonl"
-n_requests = 10_000
-jobs = [{"model": "text-embedding-ada-002", "input": str(x) + "\n"} for x in range(n_requests)]
-with open(filename, "w") as f:
-    for job in jobs:
-        json_string = json.dumps(job)
-        f.write(json_string + "\n")
